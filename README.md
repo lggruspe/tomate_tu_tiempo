c2d6ca3b740629fa3653bcf4733c57fcd16d2b1e
Levi Gruspe <mail.levig@gmail.com>
2021-07-22 18:49:18 +0800

    Add main app layout

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # tomate_tu_tiempo
 
-A new Flutter project.
+A Pomodoro timer app written in Flutter.
 
 ## Getting Started
-
-This project is a starting point for a Flutter application.
 
 A few resources to get you started if this is your first Flutter project:
 
@@ -14,3 +12,7 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## LICENSE
+
+[MIT](./LICENSE)
--- lib/main.dart
+++ lib/main.dart
@@ -4,110 +4,179 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  BodyPage _currentPage = BodyPage.Statistics;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _changePage(BodyPage page) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentPage = page;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Tómate tu tiempo'),
+          ),
+          body: MyBody(_currentPage),
+          bottomNavigationBar: MyNavBar(_currentPage, _changePage),
+        ),
+        theme: ThemeData(
+          colorScheme: ColorScheme.dark(),
+        ));
+  }
+}
+
+class MyNavBar extends StatelessWidget {
+  final BodyPage page;
+  final void Function(BodyPage) onTap;
+
+  MyNavBar(this.page, this.onTap);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      selectedItemColor: Colors.grey[400],
+      unselectedItemColor: Colors.grey[600],
+      currentIndex: page.index,
+      onTap: (index) => onTap(BodyPage.values[index]),
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.bar_chart),
+          label: 'Statistics',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.hourglass_bottom),
+          label: 'Timer',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: 'Settings',
+        ),
+      ],
+    );
+  }
+}
+
+class StatisticsBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Statistics'),
+    );
+  }
+}
+
+class SettingsBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Settings'),
+    );
+  }
+}
+
+class TimerBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        MyDescription(),
+        MyClock(),
+        MyButtons(),
+        Switch(
+          value: false,
+          onChanged: null,
+        ),
+      ],
+    );
+  }
+}
+
+class MyDescription extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.all(30.0),
+        child: Center(
+          child: const Text(
+            'A very long description of a task in my to-do list.',
+            style: TextStyle(
+              fontSize: 30.0,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class MyClock extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          // color: Colors.blue,
+          shape: BoxShape.circle,
+        ),
+        child: Center(
+          child: const Text(
+            '30:00',
+            style: TextStyle(
+              fontSize: 80.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: IconButton(
+          color: Colors.blue,
+          icon: Icon(Icons.play_arrow),
+          iconSize: 50.0,
+          onPressed: null,
+        ),
+      ),
+    );
+  }
+}
+
+enum BodyPage {
+  Statistics,
+  Timer,
+  Settings,
+}
+
+class MyBody extends StatelessWidget {
+  final BodyPage page;
+
+  MyBody(this.page);
+
+  @override
+  Widget build(BuildContext context) {
+    switch (page) {
+      case BodyPage.Statistics:
+        return StatisticsBody();
+      case BodyPage.Timer:
+        return TimerBody();
+      case BodyPage.Settings:
+        return SettingsBody();
+    }
   }
 }
